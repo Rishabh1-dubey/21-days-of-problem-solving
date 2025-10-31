38a87a28ec8cad635cf49aec764fda44b59ac5b8
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-10-31 19:52:17 +0530

    did even number question and n-1 question without changing the loop

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 //prinnt 1 to N number
 
+// let n =5;
+// for(let i =0; i<=n; i++){
+//     console.log(i)
+// }
 
-let n =5;
-for(let i =0; i<=n; i++){
-    console.log(i)
+//  print n to 1
+// let n =10;
+// for (let i=0; i<n; i++){
+//     console.log(n-i)
+// }
+
+//print all even number
+let n = 10;
+for (let i = 0; i < n/2; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
 }
